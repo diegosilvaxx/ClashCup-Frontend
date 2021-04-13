9c68fc75ffe528e40a1941edc9349e1aea38fbba
Diego Borges <diegorp2013@hotmail.com>
2021-04-13 08:09:51 -0300

    ajuste api

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import * as authActionCreators from '~/store/modules/auth/auth.actions';
 
 const apiMaster = {
   apiSistema: axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/http://back.clashcup.com.br/api/v1/',
+    baseURL: 'https://api.clashcup.com.br/api/v1/',
     // baseURL: 'http://clashcup-001-site1.itempurl.com//api/v1/',
     timeout: 1000,
     headers: { 'X-Custom-Header': 'foobar' },
